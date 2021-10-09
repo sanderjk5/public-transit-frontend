d234b18f4fe94eb8068e3f1acda438d41e0ed0af
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-10-09 18:58:48 +0200

    feat: added graph module.

---
+++ src/app/components/meat-graph/meat-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meat-graph',
+  templateUrl: './meat-graph.component.html',
+  styleUrls: ['./meat-graph.component.css']
+})
+export class MeatGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
